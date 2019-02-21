5f5130c99f6e28a5b51bc78c37eedd0f5d261f73
discdiver <jeffmshale@gmail.com>
2019-02-20 22:56:44 -0500

    fix typo in setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)' ",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
